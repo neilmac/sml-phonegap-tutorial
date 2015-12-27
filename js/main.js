57ca8e87ee537fe53dc7665309f60471527d79ee
Neil MacLennan <neil@sanctusmedia.com>
2015-12-27 20:42:55 +0000

    Create HomeView class. Add CSS.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 var app = {
 
-	renderHomeView: function() {
-	    $('body').html(this.homeTpl());
-	    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-	},
-
 	/* If navigator.notification is available, use its alert() function. Otherwise, use the default browser alert() function. */
 	showAlert: function (message, title) {
 	    if (navigator.notification) {
@@ -14,23 +9,12 @@ var app = {
 	    }
 	},
 
-    findByName: function() {
-        console.log('findByName');
-        var self = this;
-	    this.store.findByName($('.search-key').val(), function(employees) {
-	        $('.employee-list').html(self.employeeLiTpl(employees));
-	    });
-    },
-
     initialize: function() {
-	    
-	    this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
 	    
 	    var self = this;
         this.store = new LocalStorageStore(function() {
 	        //self.showAlert('LocalStore Initialised', 'Info');
-	        self.renderHomeView();
+	        $('body').html(new HomeView(self.store).render().el);
 	    });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
