f48e5167c6b50dfdb76b1fa68f9465ef149c419b
Neil MacLennan <neil@sanctusmedia.com>
2015-12-27 23:26:28 +0000

    Tap highlighting

--- js/main.js
+++ js/main.js
@@ -8,15 +8,39 @@ var app = {
 	        alert(title ? (title + ": " + message) : message);
 	    }
 	},
+	
+	registerEvents: function() {
+	    var self = this;
+	    // Check of browser supports touch events...
+	    if (document.documentElement.hasOwnProperty('ontouchstart')) {
+	        // ... if yes: register touch event listener to change the "selected" state of the item
+	        $('body').on('touchstart', 'a', function(event) {
+	            $(event.target).addClass('tappable-active');
+	        });
+	        $('body').on('touchend', 'a', function(event) {
+	            $(event.target).removeClass('tappable-active');
+	        });
+	    } else {
+	        // ... if not: register mouse events instead
+	        $('body').on('mousedown', 'a', function(event) {
+	            $(event.target).addClass('tappable-active');
+	        });
+	        $('body').on('mouseup', 'a', function(event) {
+	            $(event.target).removeClass('tappable-active');
+	        });
+	    }
+	},
 
     initialize: function() {
 	    
 	    var self = this;
         this.store = new LocalStorageStore(function() {
-	        //self.showAlert('LocalStore Initialised', 'Info');
+	        self.showAlert('LocalStore Initialised', 'Info');
 	        $('body').html(new HomeView(self.store).render().el);
 	    });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+
+        self.registerEvents();
     }
 
 };
